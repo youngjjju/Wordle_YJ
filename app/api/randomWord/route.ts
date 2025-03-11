6c2d0d2c5dc3cf03aff8daa601eabf74a44ea054
youngjju_local <2000dudwn@naver.com>
2025-03-11 19:30:37 +0900

    Refactor random word generator API to use native Response for improved consistency

--- app/api/randomWord/route.ts
+++ app/api/randomWord/route.ts
@@ -1,17 +1,12 @@
-import { NextRequest, NextResponse } from "next/server";
 import words from "@/components/fiveLengthWords";
 
-function randomWordGenarator(): string {
+export function randomWordGenarator() {
   const randomIndex = Math.floor(Math.random() * words.words.length);
   return words.words[randomIndex];
 }
 
-export const handler = {
-  GET: async (req: NextRequest) => {
-    const randomWord = randomWordGenarator();
-    console.log(randomWord);
-    console.log(req);
-
-    return NextResponse.json({ word: randomWord });
-  },
-};
+export async function GET() {
+  const randomWord = randomWordGenarator();
+  console.log(randomWord);
+  return new Response(JSON.stringify({ word: randomWord }));
+}
