2b29b5750ee99c99565244cd6119875434d5e7d0
youngjju_local <2000dudwn@naver.com>
2025-03-12 02:09:12 +0900

    Refactor WordRow component by removing unused keyboard color logic and related state management

--- components/wordRow.tsx
+++ components/wordRow.tsx
@@ -1,15 +1,6 @@
 import { useEffect, useState, useCallback } from "react";
 import WordBox from "./wordBox";
 import { answerCheck } from "./answerCheck";
-
-interface KeyboardColor {
-  [key: string]: {
-    black: boolean;
-    yellow: boolean;
-    green: boolean;
-  };
-}
-
 interface WordRowProps {
   active: boolean;
   winningGame: () => void;
@@ -20,35 +11,6 @@ interface WordRowProps {
   valid: boolean;
   setValid: React.Dispatch<React.SetStateAction<boolean>>;
 }
-
-const initialKeyboardColor: KeyboardColor = {
-  Q: { black: false, yellow: false, green: false },
-  W: { black: false, yellow: false, green: false },
-  E: { black: false, yellow: false, green: false },
-  R: { black: false, yellow: false, green: false },
-  T: { black: false, yellow: false, green: false },
-  Y: { black: false, yellow: false, green: false },
-  U: { black: false, yellow: false, green: false },
-  I: { black: false, yellow: false, green: false },
-  O: { black: false, yellow: false, green: false },
-  P: { black: false, yellow: false, green: false },
-  A: { black: false, yellow: false, green: false },
-  S: { black: false, yellow: false, green: false },
-  D: { black: false, yellow: false, green: false },
-  F: { black: false, yellow: false, green: false },
-  G: { black: false, yellow: false, green: false },
-  H: { black: false, yellow: false, green: false },
-  J: { black: false, yellow: false, green: false },
-  K: { black: false, yellow: false, green: false },
-  L: { black: false, yellow: false, green: false },
-  Z: { black: false, yellow: false, green: false },
-  X: { black: false, yellow: false, green: false },
-  C: { black: false, yellow: false, green: false },
-  V: { black: false, yellow: false, green: false },
-  B: { black: false, yellow: false, green: false },
-  N: { black: false, yellow: false, green: false },
-  M: { black: false, yellow: false, green: false },
-};
 
 export default function WordRow({
   active,
@@ -83,8 +45,6 @@ export default function WordRow({
     false,
     false,
   ]);
-  const [keyboardColor, setkeyboardColor] =
-    useState<KeyboardColor>(initialKeyboardColor);
 
   const handleKeyDown = useCallback(
     (event: KeyboardEvent) => {
@@ -137,41 +97,6 @@ export default function WordRow({
   useEffect(() => {
     setJoinedWord(word.join(""));
   }, [word]);
-
-  useEffect(() => {
-    if (!active) {
-      word.forEach((_, i) => {
-        if (keyboardColor.hasOwnProperty(word[i])) {
-          if (green[i]) {
-            setkeyboardColor((prev) => {
-              return {
-                ...prev,
-                [word[i]]: { black: false, yellow: false, green: true },
-              };
-            });
-          } else if (yellow[i]) {
-            setkeyboardColor((prev) => {
-              return {
-                ...prev,
-                [word[i]]: { black: false, yellow: true, green: false },
-              };
-            });
-          } else {
-            setkeyboardColor((prev) => {
-              return {
-                ...prev,
-                [word[i]]: { black: true, yellow: false, green: false },
-              };
-            });
-          }
-        }
-      });
-    }
-  }, [active]);
-
-  useEffect(() => {
-    console.log("Updated keyboardColor: ", keyboardColor);
-  }, [keyboardColor]); // OK
 
   return (
     <div className="w-full my-1 h-[9%] gap-2 flex justify-center items-center">
