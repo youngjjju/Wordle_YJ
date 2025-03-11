f41c10f643054c3dcbd32e856a0cbecb809ad345
youngjju_local <2000dudwn@naver.com>
2025-03-11 23:10:00 +0900

    Add scale-up animation to WordBox component on letter change

--- components/wordBox.tsx
+++ components/wordBox.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+"use client";
+import { useEffect, useState } from "react";
 
 export default function WordBox({
   index,
@@ -13,13 +14,24 @@ export default function WordBox({
   yellow: boolean;
   check: boolean;
 }) {
-  useEffect(() => {}, [yellow]);
+  const [scaleUp, setScaleUp] = useState<boolean>(false);
+  useEffect(() => {
+    if (letter !== "") {
+      setScaleUp(true);
+      setTimeout(() => {
+        setScaleUp(false);
+      }, 100);
+    } else {
+      setScaleUp(false);
+    }
+  }, [letter]);
   return (
     <div
       key={index}
-      className={`h-full w-[16%] flex text-xl md:text-2xl rounded-md justify-center items-center transition-colors duration-500  text-black ${
+      className={`h-full w-[16%] flex text-xl md:text-2xl shadow-sm rounded-md justify-center font-semibold items-center transition-colors duration-500  text-black ${
         green ? " bg-green-500" : "bg-neutral-300"
       }
+      ${scaleUp ? "scale-115 transition-all duration-200 ease-in" : ""}
       ${check && !green && !yellow ? "bg-neutral-600" : ""}
       ${yellow && !green ? " bg-yellow-500" : ""}`}
     >
