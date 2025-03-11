de4ceae33ff2c60498e907136eb8fa0792dedd12
youngjju_local <2000dudwn@naver.com>
2025-03-11 19:56:19 +0900

    Update Next.js configuration for static export and adjust package scripts

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
 };
+// next.config.js
+module.exports = {
+  output: 'export',
+}
 
 export default nextConfig;
